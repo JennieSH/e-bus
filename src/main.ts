044bfb543a0d8719dc3a54e1997dba17faf92df8
jennie.shao <m65pjj@gmail.com>
2021-11-23 22:12:22 +0800

    chore: add svg-sprite-loader package

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,10 @@ import App from "./App.vue";
 import router from "./router";
 import "@/assets/tailwind.scss";
 
+// import all svg icons
+const requireAll = (requireContext: __WebpackModuleApi.RequireContext) =>
+  requireContext.keys().map(requireContext);
+const svgIcons = require.context("./assets/icons", true, /\.svg$/);
+requireAll(svgIcons);
+
 createApp(App).use(router).mount("#app");
---
+++ vue.config.js
@@ -0,0 +1,23 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
+
+function resolve(dir) {
+  return path.join(__dirname, "./", dir);
+}
+
+module.exports = {
+  chainWebpack: (config) => {
+    config.module.rule("svg").exclude.add(resolve("src/assets/icons")).end();
+
+    config.module
+      .rule("icons")
+      .test(/\.svg$/)
+      .include.add(resolve("src/assets/icons"))
+      .end()
+      .use("svg-sprite-loader")
+      .loader("svg-sprite-loader")
+      .options({
+        symbolId: "[name]",
+      });
+  },
+};
