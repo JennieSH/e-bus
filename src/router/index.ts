cf28a66a53aafc26bd30d68195b4af610a36b323
jennie.shao <m65pjj@gmail.com>
2021-11-24 05:31:09 +0800

    feat: add Page404

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,26 +1,23 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import Home from "../views/Home.vue";
+import Home from "@/views/Home.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "Home",
-    component: Home,
+    component: Home
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    path: "/:pathMatch(.*)*",
+    name: "Page404",
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
+      import(/* webpackChunkName: "error404" */ "../views/Page404.vue")
+  }
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes,
+  routes
 });
 
 export default router;
---
+++ src/views/Page404.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="error-container">
+    <h1>Error 404</h1>
+    <h2 class="my-2 uppercase">This page doesn’t exist.</h2>
+
+    <RouterLink class="text-sm hover:text-gray-600" to="/">
+      ＜ Back to Home page
+    </RouterLink>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+.error-container {
+  @apply flex flex-col justify-center items-center;
+
+  h1 {
+    @apply mb-10 px-4 text-[3rem] text-gray-300;
+
+    animation: twinkle 0.5s alternate infinite;
+  }
+}
+
+@keyframes twinkle {
+  from {
+    box-shadow: inset -3px 0 0 rgb(255, 211, 17);
+  }
+
+  to {
+    box-shadow: inset -3px 0 0 transparent;
+  }
+}
+</style>
