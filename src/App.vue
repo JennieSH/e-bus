66ffc78ffd3b37d5c09b7dc1987edaec545e7737
jennie.shao <m65pjj@gmail.com>
2021-11-24 02:43:09 +0800

    feat: add footer

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <script setup lang="ts">
 import Header from "./components/header/Header.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
   <Header />
   <router-view />
+  <Footer />
 </template>
+
+<style lang="scss">
+#app {
+  @apply flex flex-col justify-between min-h-screen;
+}
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <footer class="py-6 font-roboto text-sm text-center text-white bg-gray-dark">
+    Copyright © 2021 台灣公車 e 點通 All rights reserved.
+  </footer>
+</template>
+
+<style lang="scss" scoped></style>
